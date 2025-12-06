57b5929a3bd557c0f48ff709c9410f19e696b644
Justin <18606523+jlewis200@users.noreply.github.com>
2025-12-06 11:25:19 -0500

    Add day 6 solutions.

--- 6/solve.py
+++ 6/solve.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python3
 
-import re
-import math
-import itertools
 import numpy as np
-import networkx as nx
 
 
 def solve(parsed):
-    breakpoint()
+    operations = parsed[-1]
+    operand_groups = get_operand_groups(parsed[:-1])
+    results = []
+
+    for operation, operands in zip(operations, operand_groups):
+        results.append(perform_operation(operation, operands))
+
+    return sum(results)
+
+
+def perform_operation(operation, operands):
+    if operation == "*":
+        return np.prod(operands)
+
+    return sum(operands)
+
+
+def get_operand_groups(operands):
+    return np.array(operands).astype(int).T
 
 
 def parse(lines):
     parsed = []
 
     for line in lines:
-        parsed.append(line)  # do something more useful here
+        parsed.append(line.split())
 
     return parsed
 
@@ -33,5 +47,5 @@ def main(filename, expected=None):
 
 
 if __name__ == "__main__":
-    main("test_0.txt", None)
+    main("test_0.txt", 4277556)
     main("input.txt")
